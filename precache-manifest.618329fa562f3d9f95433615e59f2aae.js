387e586059585c1b58e06b9e8ae478b63910027b
fjulien <fettingerjulien@yahoo.fr>
2018-11-15 18:44:29 +0100

    Updates

---
+++ precache-manifest.618329fa562f3d9f95433615e59f2aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82fa840b5c49f36192ed",
+    "url": "/list_course/static/js/runtime~main.82fa840b.js"
+  },
+  {
+    "revision": "cd4c9fefbc7057b80940",
+    "url": "/list_course/static/js/main.cd4c9fef.chunk.js"
+  },
+  {
+    "revision": "f4d40112bcaa263b227a",
+    "url": "/list_course/static/js/1.f4d40112.chunk.js"
+  },
+  {
+    "revision": "cd4c9fefbc7057b80940",
+    "url": "/list_course/static/css/main.fdb45d9a.chunk.css"
+  },
+  {
+    "revision": "f4d40112bcaa263b227a",
+    "url": "/list_course/static/css/1.9621b891.chunk.css"
+  },
+  {
+    "revision": "27900fad41b7e088e1a4e6597a176da0",
+    "url": "/list_course/index.html"
+  }
+];
